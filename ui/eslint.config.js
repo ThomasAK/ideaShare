70cb9b8c40feec0fe2814787b9492ca65b3676cf
rkempton <r@snow87.com>
2024-09-08 14:00:27 -0600

    move ts-standard flat config to separate library

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -1,65 +1,12 @@
-import eslintConfigStandard from 'eslint-config-standard'
-import standardts from 'eslint-config-standard-with-typescript'
+import tsStandardEslintFlatConfig from 'ts-standard-eslint-flat-config'
 import globals from 'globals'
-import importPlugin from 'eslint-plugin-import'
-import n from 'eslint-plugin-n'
-import promise from 'eslint-plugin-promise'
-import tslint from '@typescript-eslint/eslint-plugin'
-import tsparser from '@typescript-eslint/parser'
-delete standardts.extends
-
-function toLanguageOptions (conf) {
-  conf.languageOptions = {
-    parserOptions: conf.parserOptions || {
-      project: true
-    }
-  }
-  delete conf.parser
-  delete conf.plugins
-  delete conf.globals
-  delete conf.env
-  delete conf.parserOptions
-}
-
-toLanguageOptions(eslintConfigStandard)
-toLanguageOptions(standardts)
-
-standardts.languageOptions.parser = {
-  ...tsparser,
-  parse: (...args) => {
-    debugger
-    return tsparser.parse(...args)
-  },
-  parseForESLint: (...args) => {
-    debugger
-    return tsparser.parseForESLint(...args)
-  }
-}
-
 export default [
   {
     ignores: ['**/dist/**', '**/**js']
   },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.jsx',
-    ],
-  },
-  eslintConfigStandard,
-  standardts,
-  {
-    plugins: {
-      import: importPlugin,
-      n,
-      promise,
-      '@typescript-eslint': tslint
-    },
-    languageOptions: {
+  ...tsStandardEslintFlatConfig,
+  {languageOptions: {
       globals: {
-        ...globals.es2021,
-        ...globals.node,
         ...globals.browser
       }
     }
