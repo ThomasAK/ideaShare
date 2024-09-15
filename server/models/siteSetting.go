70fa409da78467d793893af20920d6f7a01440ca
rkempton <r@snow87.com>
2024-09-14 21:16:37 -0600

    make settings unique make settings keys varchar to make creating a unique index work

--- server/models/siteSetting.go
+++ server/models/siteSetting.go
@@ -2,6 +2,6 @@ package models
 
 type SiteSetting struct {
 	SoftDeleteModel
-	Key   string `gorm:"not null" json:"name"`
+	Key   string `gorm:"uniqueIndex;not null;type:varchar(255)" json:"name"`
 	Value string `gorm:"not null" json:"value"`
 }
--- server/models/userSetting.go
+++ server/models/userSetting.go
@@ -2,7 +2,7 @@ package models
 
 type UserSetting struct {
 	SoftDeleteModel
-	Key    string `gorm:"not null" json:"key"`
+	Key    string `gorm:"not null;index:user_settings_user_id_key_unq,unique;type:varchar(255)" json:"key"`
 	Value  string `gorm:"not null" json:"value"`
-	UserId int    `gorm:"not null;index" json:"userId" faker:"-"`
+	UserId int    `gorm:"not null;index:user_settings_user_id_key_unq,unique" json:"userId" faker:"-"`
 }
